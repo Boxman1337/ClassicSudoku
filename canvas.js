0ec2ff138e55220d26c0fdd4c29737ef4a99db3e
Boxman1337 <47634055+Boxman1337@users.noreply.github.com>
2023-12-15 04:13:28 +0100

    Simple game-loop finished

--- canvas.js
+++ canvas.js
@@ -5,13 +5,35 @@ class Canvas {
         this.canvas = document.getElementById("canvas");
         this.canvasContext = this.canvas.getContext("2d");
 
+        this.selectedCell = {x: 0, y: 0};
+        //this.selectedCell = {x: 0, y: 0};
+
         this.cellColor = "#a89984";
         this.borderColor = "#7c6f64";
         this.borderColorDark = "#665c54";
         this.textColor = "#ebdbb2";
+        this.selectedCellColor = "rgba(181, 118, 20, 0.4)";
 
         this.sideSize = this.canvas.width / 9;
         this.wallWidth = this.sideSize / 50;
+    }
+
+    drawSelectedCell() {
+
+        let selectedCell = this.selectedCell;
+        let sideSize = this.sideSize;
+
+        if (selectedCell != null) {
+            let offset = this.sideSize * 0.03;
+            
+            let xPos = selectedCell.x * sideSize + offset;
+            let yPos = selectedCell.y * sideSize + offset;
+
+            let width = this.sideSize - 2 * offset;
+            let height = this.sideSize - 2 * offset;
+
+            this.drawRect(xPos, yPos, width, height, this.selectedCellColor);    
+        }
     }
 
     drawRect(x, y, width, height, color) {
@@ -30,7 +52,7 @@ class Canvas {
         this.drawRect(6 * sideSize, 0, 3 * wallWidth, 9 * sideSize, this.borderColorDark);
     }
 
-    drawGrid(matrix) {
+    drawCells(matrix) {
 
         let sideSize = this.sideSize;
         let wallWidth = this.wallWidth;
@@ -82,7 +104,17 @@ class Canvas {
                     wallWidth + sideSize,
                     this.borderColor
                 );
-    
+            }
+        }
+    }
+
+    drawCellNumber(matrix) {
+
+        let sideSize = this.sideSize;
+
+        for (let row = 0; row < matrix.length; row++) {
+            for (let column = 0; column < matrix[0].length; column++) {
+                
                 // Draw Cell Number
                 this.canvasContext.font = "36px Arial"
                 this.canvasContext.fillStyle = this.textColor;
@@ -96,6 +128,23 @@ class Canvas {
                 }
             }
         }
+    }
+
+    drawGrid(matrix) {
+
+        let sideSize = this.sideSize;
+        let wallWidth = this.wallWidth;
+
+        // Draw cells and cell walls for each cell
+        this.drawCells(matrix);
+
+        // Draw selected cell if any
+        this.drawSelectedCell();
+
+        // Draw cell number for each cell
+        this.drawCellNumber(matrix);
+
+        
     }
 }
 
